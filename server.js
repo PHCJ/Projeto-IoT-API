951b771a67abe746bacc9bc0cf3b568585336dc6
Paulo H. C. Jesus <paulo.jesus2@fatec.sp.gov.br>
2023-09-18 12:15:04 -0300

    port server

--- server.js
+++ server.js
@@ -6,3 +6,8 @@ const app = express()
 app.get("/", function (req, res) {
     res.send("Projeto Esp - Daniela e Paulo")
 })
+
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
